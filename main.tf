0bedd48543c73b39093875f0af84b3b912340e9e
ytkayamura <yutaka.ayamura@beeskynet.com>
2025-02-18 00:16:50 +0900

    API Gateway追加

--- main.tf
+++ main.tf
@@ -47,3 +47,49 @@ resource "aws_lambda_function" "hello_world" {
 
   filename = "lambda.zip"
 }
+
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hello_world.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.hello_world_api.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_rest_api" "hello_world_api" {
+  name        = "HelloWorldAPI"
+  description = "API Gateway for HelloWorld Lambda"
+}
+
+resource "aws_api_gateway_resource" "hello_world_resource" {
+  rest_api_id = aws_api_gateway_rest_api.hello_world_api.id
+  parent_id   = aws_api_gateway_rest_api.hello_world_api.root_resource_id
+  path_part   = "hello"
+}
+
+resource "aws_api_gateway_method" "hello_world_method" {
+  rest_api_id   = aws_api_gateway_rest_api.hello_world_api.id
+  resource_id   = aws_api_gateway_resource.hello_world_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "hello_world_integration" {
+  rest_api_id = aws_api_gateway_rest_api.hello_world_api.id
+  resource_id = aws_api_gateway_resource.hello_world_resource.id
+  http_method = aws_api_gateway_method.hello_world_method.http_method
+  type        = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri         = aws_lambda_function.hello_world.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "hello_world_deployment" {
+  depends_on = [aws_api_gateway_integration.hello_world_integration]
+  rest_api_id = aws_api_gateway_rest_api.hello_world_api.id
+  stage_name  = "prod"
+}
+
+output "api_gateway_url" {
+  value = "${aws_api_gateway_deployment.hello_world_deployment.invoke_url}/hello"
+  description = "The URL of the API Gateway"
+}
